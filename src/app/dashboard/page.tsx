4bf4787a0201fd0348cf6a333864779d105e7531
kshitij-hash <83551355+kshitij-hash@users.noreply.github.com>
2024-06-01 15:05:18 +0530

    changes in metadata and layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,12 @@ import { redirect } from 'next/navigation'
 
 import { createClient } from '@/utils/supabase/server'
 import StudentDetails from '@/components/studentDetails'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: "Dashboard",
+    description: "Dashboard for the admin to manage the student details.",
+}
 
 export default async function Dashboard() {
     const supabase = createClient();
