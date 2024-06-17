b41420d7c2138fa3d0014b37285879a972c3f158
kshitij-hash <83551355+kshitij-hash@users.noreply.github.com>
2024-06-18 01:32:51 +0530

    removed unused code

--- src/components/editDialog.tsx
+++ src/components/editDialog.tsx
@@ -5,59 +5,21 @@ import {
     DialogContent,
     DialogTrigger,
 } from "@/components/ui/dialog"
-import { createClient } from "@/utils/supabase/client";
-import { Edit, Trash2 } from "lucide-react"
-import { useToast } from "./ui/use-toast";
+import { Edit } from "lucide-react"
 import { useState } from "react";
 import EditStudentForm from "./editStudentForm";
 
-type DeleteDialogProps = {
+type EditDialogProps = {
     id: string
     handleFormSubmit: () => void
 }
 
-export default function EditDialog({ id, handleFormSubmit }: DeleteDialogProps) {
+export default function EditDialog({ id, handleFormSubmit }: EditDialogProps) {
     const [open, setOpen] = useState(false);
-    const [loading, setLoading] = useState(false);
 
     const handleClose = () => {
         setOpen(false);
     }
-
-    const { toast } = useToast();
-    const handleDelete = async (e: { preventDefault: () => void; }) => {
-        setLoading(true);
-        e.preventDefault();
-        const supdabase = createClient();
-
-        try {
-            const response = await supdabase
-                .from('Students')
-                .delete()
-                .eq('id', id);
-
-            console.log(response);
-            if (response.status === 204) {
-                toast({
-                    title: 'Student record deleted',
-                    description: 'The student record has been deleted successfully.',
-                })
-            } else {
-                toast({
-                    title: 'An error occurred',
-                    description: `${response.error?.message}` || 'An error occurred while deleting the student record.',
-                    variant: 'destructive'
-                })
-            }
-            handleClose();
-            handleFormSubmit();
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setLoading(false);
-        }
-    }
-
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>
